16676a40bd99c118a33c3be6a34cada0bb84f7b8
leepika <kottakotaleepika107@gmail.com>
2025-11-15 15:33:11 +0530

    fix defineConfig error for Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig = {
   experimental: {
     turbopack: true,
     outputFileTracingRoot: path.join(__dirname, 'src'),
-    turbopack: {
-      root: path.join(__dirname, 'src'),
-    },
   },
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
